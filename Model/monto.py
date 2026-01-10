2027162a306b8bf152777a4d9a70dc9480733030
albcv <albcvad@gmail.com>
2026-01-10 09:50:37 -0500

    Agregando métodos get a la clase Monto

--- Model/monto.py
+++ Model/monto.py
@@ -16,3 +16,15 @@ class Monto:
         
     def convertir(self, tipo : str):
         pass
+    
+    
+    def cantidad(self):
+        return self.__cantidad
+
+
+    def tipo(self):
+        return self.__tipo
+    
+    
+    def __str__(self):
+        return f"{self.__cantidad} {self.__tipo}" 
