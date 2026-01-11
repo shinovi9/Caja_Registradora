a51710675441ba25b49ca6fa4a48e236ed28b272
shinovi9 <tato22222222@outlook.com>
2026-01-11 19:03:41 -0400

    Declaracion de los Metodos y Atributos principales de la clase Tasa y importado del modulo json

--- Model/tasa.py
+++ Model/tasa.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
-
+import json
 class Tasa:
-    pass
+    __tasa_Cambio : dict
+    
+    @staticmethod
+    def __init__(self):
+        pass
+    
+    @staticmethod
+    def get_valor(tipo : str)-> float:
+        pass
+    @staticmethod
+    def get_tipo()-> tuple:
+        pass
