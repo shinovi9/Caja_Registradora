d200e06737e4ad6656ffc3cae513c43b19e247e4
shinovi9 <tato22222222@outlook.com>
2026-01-11 19:50:28 -0400

    Documentacion de Metodos valor y tipo, en la clase Tasa

--- Model/tasa.py
+++ Model/tasa.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python3
 import json
+
 class Tasa:
     __tasa_Cambio : dict
     
     @staticmethod
-    def __cargar_Data(self):
+    def __cargar_Data():
         pass
     
     @staticmethod
     def valor(tipo : str)-> float:
+        """## Otener el Valor actual de una moneda en la Tasa de canbio
+        Args:
+            tipo (str): tipo de moneda
+        Returns:
+            float: Valor actual de la moneda
+        """
         return Tasa.__tasa_Cambio[tipo]
     
     @staticmethod
     def tipo()-> tuple:
+        """### Obtener los Tipos de Monedas disponibles en la tasa de Cambio
+        Returns:
+            tuple: Todas la monedas disponibles
+        """
         return tuple(Tasa.__tasa_Cambio.keys())
