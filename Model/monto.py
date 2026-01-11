a918d9da2f6342f82e6bfd9f7689954ed93b5e0f
shinovi9 <tato22222222@outlook.com>
2026-01-10 21:55:39 -0400

    refinamiento de los metodos __init__ y convertir

--- Model/monto.py
+++ Model/monto.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
 
 class Monto:
-    def __init__(self, cantidad, tipo):
+    def __init__(self, cantidad : float, tipo : str):
+        """
+        Docstring para __init__
+        
+        :param self: Descripción
+        :param cantidad: Cantidad del Monto
+        :type cantidad: float 
+        :param tipo: tipo de Monto
+        :type tipo: str
+        """
         self.cantidad = cantidad
         self.tipo = tipo
         
-    def convertir(self, tipo):
+    def convertir(self, tipo : str):
         pass
