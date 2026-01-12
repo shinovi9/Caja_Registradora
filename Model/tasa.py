b83107b4f89240d7b9721b69c4a47c6485b0ecd8
shinovi9 <tato22222222@outlook.com>
2026-01-11 22:58:08 -0400

    Implimentacion del Metodo __cargar_Data, en la clase Tasa

--- Model/tasa.py
+++ Model/tasa.py
@@ -2,11 +2,21 @@
 import json
 
 class Tasa:
+    
     __tasa_Cambio : dict
     
     @staticmethod
     def __cargar_Data():
-        pass
+        """## Carga de la Base de Datos las Tasas de cambio 
+        Returns:
+            dict: La tasa de cambio
+        """
+        tasa : dict
+        with open("./Caja_Registradora/Data/baseDatos_Tasas.json","r") as db_Tasas:
+            tasa = json.load(db_Tasas)
+        return tasa
+    
+    __tasa_Cambio = __cargar_Data()
     
     @staticmethod
     def valor(tipo : str)-> float:
