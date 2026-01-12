4662f03f0ade7a02cabfde9f511d78fa7c188f9b
altorarreiss-source <altorarreiss@gmail.com>
2026-01-12 08:53:48 -0500

    Agregando el layout de la clase View

---
+++ View/View.py
@@ -0,0 +1,21 @@
+# console_view.py
+class View:
+    def Mostrar_Menú(self):
+        print("\n Menú Principal")
+        print("1. Opción 1")
+        print("2. Opción 2")
+        print("3. Opción 3")
+    def Opción_Seleccionada(self):
+        return input("Por favor, selecciona una opción: ")
+
+    def Mostrar_Mensaje(self, mensaje):
+        print(f"\n{mensaje}")
+
+if __name__ == "__main__":
+    """## Ejecutar el código siguiente si el archivo View está siendo probado como principal(main)
+    Si la clase View va a ser ejecutado desde main.py, el código a partir de aquí no se ejecuta"""
+    print("Provando la Vista...")
+    view = View()
+    view.Mostrar_Menú()
+    selección = view.Opción_Seleccionada()
+    view.Mostrar_Mensaje(f"Seleccionaste: {selección}")
