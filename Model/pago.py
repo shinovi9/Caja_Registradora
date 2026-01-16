b23c6fa6f00021500c80445ddbefc7fe4672d1c8
shinovi9 <tato22222222@outlook.com>
2026-01-15 22:06:30 -0400

    Restructuracion de la clase Pago, implementacion parcial de __procesar

--- Model/pago.py
+++ Model/pago.py
@@ -14,43 +14,44 @@ class Pago:
         pass
     
     @staticmethod
-    def __procesar(deudor : str, montos : list)-> dict:
-        pass
-    
-    @staticmethod
-    def __consultar_deuda(deudor : str, tipo : str = ''):
-        """### Optiene la informacion de las deudas  
+    def __procesar(costos: tuple, montos : list)-> dict:
+        
+        restante_monto : float
+        tamaño_total_Monto : float
+        montos_xTipos : dict
+        montos_finales : list
+        costos_faltante : tuple
+        
 
-        Args:
-            deudor (str): Persona que debe el dinero
-            tipo (str, optional): Moneda de la Deuda. Defaults to ''.
-        Raises:
-            FileNotFoundError: Si no se encuentra el archivo Deudores.json
-        Returns:
-        tuple:
-            - Si no se encuentra el Deudor → (deudor, 0)
-            - Si se especifica el tipo → (tipo, monto_de_la_deuda)
-            - Si no, se devuelve todas las deudas → (deuda,)
-        """
-        deuda : dict
-        ruta = Path("./Caja_Registradora/Data/baseDatos_Deuda/Deudores.json")
+        tipo_destinado = costos[0]
+        tasa_destino = Tasa.valor(tipo_destinado)
         
-        if not ruta.exists():
-            raise FileNotFoundError(f"No se encontró el archivo: {ruta}")
+        for monto in montos: # Separar los tipos de Monto
+            if monto.tipo() not in montos_xTipos:
+                montos_xTipos[monto.tipo()] = [monto,]
+            else:
+                montos_xTipos[monto.tipo()].append(monto)
+        # Agrupacion de los montos del tipo a la deudea destinado
+        if tipo_destinado in montos_xTipos:
+            montos_finales = [m for m in montos_xTipos[tipo_destinado]]
+        # Calculo del tamaño_total_monto
+        costos_faltante : tuple = (tipo_destinado, 0.0)
         
-        with open(ruta, "r", encoding="utf-8") as f:
-            data = json.load(f)
-            if deudor not in data:
-                return (deudor,0)
-            deuda = data[deudor]
-            
-        if tipo and tipo in deuda :
-            return (tipo, deuda[tipo])
-        return (deuda,)
+        if len(montos_xTipos.keys()) > 1:
+            pass
+        else: # en caso de no tener aque convertir montos
+            for vM in montos_finales:
+                tamaño_total_Monto += vM.valor()            
+        restante_monto = costos[1] - tamaño_total_Monto
+        if restante_monto <= 0 :
+            costos_faltante = (tipo_destinado, 0.0)
+            if restante_monto < 0:
+                restante_monto -= restante_monto*2
+        elif restante_monto > 0:
+            costos_faltante = (tipo_destinado, restante_monto)
+        pago_exitoso = True if costos_faltante == 0.0 else False
         
-    @staticmethod
-    def __actualizar_deuda(dudor : str, monto_ : tuple):
-        pass
+        return {"pago_exitoso" : pago_exitoso ,"sobrante" : restante_monto, "faltante" : costos_faltante}
     
     @staticmethod
     def __main(args : dict)-> dict:
